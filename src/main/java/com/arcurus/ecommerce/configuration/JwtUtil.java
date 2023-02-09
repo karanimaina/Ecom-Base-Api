da303dcfa9fb3e7b65adef791a400a78390add2f
karanimaina <karani.maina2010@gmail.com>
2023-02-09 23:57:49 +0300

    add Jwt util

--- src/main/java/com/arcurus/ecommerce/configuration/JwtUtil.java
+++ src/main/java/com/arcurus/ecommerce/configuration/JwtUtil.java
@@ -16,6 +16,7 @@ import java.util.function.Function;
 @Component
 public class JwtUtil {
 
+
     private static  final String SECRET_KEY="secret";
     private static  final int TOKEN_VALIDITY=3600*5;
     public String getUserNameFromToken(String token){
