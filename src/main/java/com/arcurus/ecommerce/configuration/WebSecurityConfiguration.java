4455533a14401196acceaab9c3fcb5951e895973
karanimaina <karani.maina2010@gmail.com>
2023-02-09 23:55:51 +0300

    add Jwt util

--- src/main/java/com/arcurus/ecommerce/configuration/WebSecurityConfiguration.java
+++ src/main/java/com/arcurus/ecommerce/configuration/WebSecurityConfiguration.java
@@ -31,7 +31,6 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 private final JwtRequestFilter jwtRequestFilter;
 
 
-
 private  UserDetailsService jwtService;
   private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
 
