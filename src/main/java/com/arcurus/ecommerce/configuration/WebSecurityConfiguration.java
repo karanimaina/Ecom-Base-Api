ba0fcfe183031747b3b26e734a0c947e90ea7bcd
karanimaina <karani.maina2010@gmail.com>
2023-02-09 23:54:02 +0300

    add Jwt util

--- src/main/java/com/arcurus/ecommerce/configuration/WebSecurityConfiguration.java
+++ src/main/java/com/arcurus/ecommerce/configuration/WebSecurityConfiguration.java
@@ -1,71 +1,71 @@
-//package com.arcurus.ecommerce.configuration;
-//
-//import lombok.RequiredArgsConstructor;
-//import lombok.experimental.WithBy;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.context.annotation.Lazy;
-//import org.springframework.http.HttpHeaders;
-//import org.springframework.security.authentication.AuthenticationManager;
-//import org.springframework.security.config.BeanIds;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-//import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.config.http.SessionCreationPolicy;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.security.web.AuthenticationEntryPoint;
-//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-//
-//@Configuration
-//@EnableWebSecurity
-//@RequiredArgsConstructor
-//@EnableGlobalMethodSecurity(prePostEnabled = true)
-//
-//public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
-//private final JwtRequestFilter jwtRequestFilter;
-//
-//private  UserDetailsService jwtService;
-//  private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
-//
+package com.arcurus.ecommerce.configuration;
+
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.WithBy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+private final JwtRequestFilter jwtRequestFilter;
+
+private  UserDetailsService jwtService;
+  private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+
+  @Override
+  protected  void configure(HttpSecurity httpSecurity) throws Exception{
+    httpSecurity.cors();
+    httpSecurity.csrf().disable()
+            .authorizeRequests().antMatchers("/authenticate")
+            .permitAll()
+            .antMatchers(HttpHeaders.ALLOW)
+            .permitAll()
+            .anyRequest()
+            .authenticated()
+            .and()
+            .exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint)
+            .and()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+    httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+  }
+  @Bean
+  public PasswordEncoder passwordEncoder(){
+    return new BCryptPasswordEncoder();
+  }
+  @Bean
+  @Override
+  public AuthenticationManager authenticationManager() throws Exception {
+    return super.authenticationManager();
+  }
+@Autowired
+  public void configureGlobal(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+    authenticationManagerBuilder.userDetailsService(jwtService).passwordEncoder(passwordEncoder());
+  }
+//  @Bean(name = BeanIds.AUTHENTICATION_MANAGER)
 //  @Override
-//  protected  void configure(HttpSecurity httpSecurity) throws Exception{
-//    httpSecurity.cors();
-//    httpSecurity.csrf().disable()
-//            .authorizeRequests().antMatchers("/authenticate")
-//            .permitAll()
-//            .antMatchers(HttpHeaders.ALLOW)
-//            .permitAll()
-//            .anyRequest()
-//            .authenticated()
-//            .and()
-//            .exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint)
-//            .and()
-//            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-//
-//    httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+//  public AuthenticationManager authenticationManagerBean() throws Exception {
+//    return super.authenticationManagerBean();
 //  }
-//  @Bean
-//  public PasswordEncoder passwordEncoder(){
-//    return new BCryptPasswordEncoder();
-//  }
-//  @Bean
-//  @Override
-//  public AuthenticationManager authenticationManager() throws Exception {
-//    return super.authenticationManager();
-//  }
-//@Autowired
-//  public void configureGlobal(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
-//    authenticationManagerBuilder.userDetailsService(jwtService).passwordEncoder(passwordEncoder());
-//  }
-////  @Bean(name = BeanIds.AUTHENTICATION_MANAGER)
-////  @Override
-////  public AuthenticationManager authenticationManagerBean() throws Exception {
-////    return super.authenticationManagerBean();
-////  }
-//}
+}
