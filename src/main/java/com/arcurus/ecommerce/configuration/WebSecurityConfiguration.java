ab35770480d50af9c74f77cb23bb5544372dd621
karanimaina <karani.maina2010@gmail.com>
2023-02-09 23:54:11 +0300

    add Jwt util

--- src/main/java/com/arcurus/ecommerce/configuration/WebSecurityConfiguration.java
+++ src/main/java/com/arcurus/ecommerce/configuration/WebSecurityConfiguration.java
@@ -30,6 +30,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 private final JwtRequestFilter jwtRequestFilter;
 
+
 private  UserDetailsService jwtService;
   private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
 
